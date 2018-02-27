1f0bbe6ccb84afd583bfb0dec249cdd169504476
JoonaNiemela <jonttuniemela@hotmail.com>
2018-02-27 14:49:14 +0200

    "lisaa-annos"-sivun saa nyt auki, get tehtynä

--- src/main/java/tikape/runko/domain/Main.java
+++ src/main/java/tikape/runko/domain/Main.java
@@ -64,34 +64,34 @@ public class Main {
             return new ThymeleafTemplateEngine().render(new ModelAndView(map, "keittokirja"));
         });
     
-//        Spark.get("/annoslistaus/:id", (req, res) -> {
-//            HashMap map = new HashMap<>();
-//
-//            map.put("annos", annoksetDao.findOne(Integer.parseInt(req.params("id"))));
-//
-//            return new ModelAndView(map, "annos");
-//        }, new ThymeleafTemplateEngine());
-//           
+        Spark.get("/annoslistaus/:id", (req, res) -> {
+            HashMap map = new HashMap<>();
 
-//        // RAAKA-AINEET
-//        // Haetaan kaikki raaka-aineet
-//        Spark.get("/haeaineet", (req, res) -> {
-//
-//            HashMap<String, Object> map = new HashMap();
-//            map.put("raakaaineet", raakaaineetDao.findAll());
-//
-//            return new ModelAndView(map, "lisaaaine");
-//        }, new ThymeleafTemplateEngine());
-//
-//        // Lisätään raaka-aine
-//        Spark.post("/lisaaaine", (req, res) -> {
-//
-//            Raakaaine raakaaine = new Raakaaine(-1, req.queryParams("nimi"));
-//            raakaaineetDao.saveOrUpdate(raakaaine);
-//
-//            res.redirect("/lisaaaine");
-//            return "";
-//        });
+            map.put("annos", annoksetDao.findOne(Integer.parseInt(req.params("id"))));
+
+            return new ModelAndView(map, "annos");
+        }, new ThymeleafTemplateEngine());
+           
+
+        // RAAKA-AINEET
+        // Haetaan kaikki raaka-aineet
+        Spark.get("/haeaineet", (req, res) -> {
+
+            HashMap<String, Object> map = new HashMap();
+            map.put("raakaaineet", raakaaineetDao.findAll());
+
+            return new ModelAndView(map, "lisaaaine");
+        }, new ThymeleafTemplateEngine());
+
+        // Lisätään raaka-aine
+        Spark.post("/lisaaaine", (req, res) -> {
+
+            Raakaaine raakaaine = new Raakaaine(-1, req.queryParams("nimi"));
+            raakaaineetDao.saveOrUpdate(raakaaine);
+
+            res.redirect("/lisaaaine");
+            return "";
+        });
 //
 //        // Poistetaan yksittäinen raaka-aine
 //        Spark.post("/poistaraakaaine", (req, res) -> {
@@ -106,25 +106,25 @@ public class Main {
 //        });
 //
 //        
-//        // ANNOKSET
-//        // Haetaan kaikki annokset
-//        Spark.get("/annoslistaus", (req, res) -> {
-//
-//            HashMap<String, Object> map = new HashMap();
-//            map.put("annokset", annoksetDao.findAll());
-//
-//            return new ModelAndView(map, "annokset");
-//        }, new ThymeleafTemplateEngine());
-//
-//        // Lisätään annos
-//        Spark.post("/annoslistaus", (req, res) -> {
-//
-//            Annos annos = new Annos(-1, req.queryParams("nimi"));
-//            annoksetDao.saveOrUpdate(annos);
-//
-//            res.redirect("/annoslistaus");
-//            return "";
-//        });
+        // ANNOKSET
+        // Haetaan kaikki annokset
+        Spark.get("/annoslistaus", (req, res) -> {
+
+            HashMap<String, Object> map = new HashMap();
+            map.put("annokset", annoksetDao.findAll());
+
+            return new ModelAndView(map, "annokset");
+        }, new ThymeleafTemplateEngine());
+
+        // Lisätään annos
+        Spark.post("/annoslistaus", (req, res) -> {
+
+            Annos annos = new Annos(-1, req.queryParams("nimi"));
+            annoksetDao.saveOrUpdate(annos);
+
+            res.redirect("/annoslistaus");
+            return "";
+        });
 //
 //        // Poistetaan annos
 //        Spark.post("/poistaannos", (req, res) -> {
@@ -171,64 +171,47 @@ public class Main {
 //            return "";
 //        });       
 //        
-//        // Lisää annos “/lisaa-annos”
-//        Spark.get("/lisaaannos", (req, res) -> {
-//
-//            HashMap<String, Object> map = new HashMap();
-//            List<String> raakaaineett = new ArrayList();
-//
-//            // yhteys raaka-ainetietokantaan, osoite puuttuu
-//            Database databaseRaakaaine = new Database("osoite");
-//            Connection con = databaseRaakaaine.getConnection();
-//
-//            // Etsitään raaka-aineiden nimet
-//            PreparedStatement stmt = con.prepareStatement("SELECT nimi FROM Raaka-aineet");
-//
-//            ResultSet tulos = stmt.executeQuery();
-//
-//            // käsittele kyselyn tulokset
-//            while (tulos.next()) {
-//                String nimi = tulos.getString("nimi");
-//                raakaaineett.add(nimi);
-//            }
-//
-//            // Lista jo laitetuista raaka-aineista                                      *
-//            // raakaaineohje-tietokannasta pitää etsiä tämän annoksen raaka-aineet      *
-//            List<RaakaaineAnnos> joLaitetut = new ArrayList();
-//
-//            map.put("raakaaineet", raakaaineett);
-//            map.put("laitetut", joLaitetut);
-//
-//            return new ThymeleafTemplateEngine().render(new ModelAndView(map, "Path to template"));
-//        });
+//         Lisää annos “/lisaa-annos”
+        Spark.get("/lisaa-annos", (req, res) -> {
+
+            HashMap<String, Object> map = new HashMap();
+            List<Raakaaine> raakaaineett = new ArrayList();
+            List<RaakaaineAnnos> jolaitetut = new ArrayList();
+
+            raakaaineett = raakaaineetDao.findAll();
+            jolaitetut = raakaaineannoksetDao.findAll();
+
+
+            return new ThymeleafTemplateEngine().render(new ModelAndView(map, "lisaaannos"));
+        });
 //
 //        Spark.post("/lisaa-annos", (req, res) -> {
-////
-////            // Haetaan Thymeleafilta tiedot Thymeleafin nimeämisten mukaan
-////            String raakaaine = req.queryParams("raakaaine");
-////            String maara = req.queryParams("maara");
-////            String lisaohje = req.queryParams("lisaohje");
-////
-////            // haetaan nimen perusteella raaka-aine, jos tarvetta?                      *
-////            // Raakaaine raakaaine2 = tarvittavametodi(raakaaine);
-////            
-////            Raakaaine raakaaine2 = 
-////
-////            // Javaan uusi raaka-aineohje, joka myöhemmin lisätään tietokantaan
-////            RaakaaineAnnos raakaaineohje = new RaakaaineAnnos(raakaaine2, maara, lisaohje);
-////            List<RaakaaineAnnos> raakaaineohjeet = new ArrayList();
-////            raakaaineohjeet.add(raakaaineohje);
-////
-////            // Järjestyksen laskeminen ja laittaminen?
-////            raakaaineohje.setJarjestys(raakaaineohjeet.size());
-////
-////            // Tietokantaan laittaminen
-////            
-////            
-////            
-////            
-////            // Sivun päivittäminen
-////            res.redirect("/lisaa-annos");
+//
+//            // Haetaan Thymeleafilta tiedot Thymeleafin nimeämisten mukaan
+//            String raakaaine = req.queryParams("raakaaine");
+//            String maara = req.queryParams("maara");
+//            String lisaohje = req.queryParams("lisaohje");
+//
+//            // haetaan nimen perusteella raaka-aine, jos tarvetta?                      *
+//            // Raakaaine raakaaine2 = tarvittavametodi(raakaaine);
+//            
+//            Raakaaine raakaaine2 = 
+//
+//            // Javaan uusi raaka-aineohje, joka myöhemmin lisätään tietokantaan
+//            RaakaaineAnnos raakaaineohje = new RaakaaineAnnos(raakaaine2, maara, lisaohje);
+//            List<RaakaaineAnnos> raakaaineohjeet = new ArrayList();
+//            raakaaineohjeet.add(raakaaineohje);
+//
+//            // Järjestyksen laskeminen ja laittaminen?
+//            raakaaineohje.setJarjestys(raakaaineohjeet.size());
+//
+//            // Tietokantaan laittaminen
+//            
+//            
+//            
+//            
+//            // Sivun päivittäminen
+//            res.redirect("/lisaa-annos");
 //
 //            return "";
 //        });
